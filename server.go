a940046864080c8b3b0bf67fafbfaad576736266
zxs-000202 <2635718400@qq.com>
2023-09-15 19:37:23 +0800

    第七次课：超时强踢功能

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -51,6 +52,9 @@ func (this *Server) Handler(conn net.Conn) {
 	//上线业务
 	user.Online()
 
+	//监听用户是否活跃的channel
+	isLive := make(chan bool)
+
 	//接受客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
@@ -69,10 +73,31 @@ func (this *Server) Handler(conn net.Conn) {
 
 			//用户针对msg的消息进行处理
 			user.DoMessage(msg)
+
+			//用户的任意消息代表用户是活跃的
+			isLive <- true
 		}
 	}()
 
-	select {}
+	for {
+		select {
+		case <-isLive:
+			//当前用户活跃，重置定时器
+			//不做任何事情，为了激活select，更新下面的定时器
+		case <-time.After(time.Second * 10):
+			//已经超时，将当前的user强制关闭
+			user.SendMsg("你被踢了")
+
+			//销毁用户的资源
+			close(user.C)
+
+			//关闭连接
+			conn.Close()
+
+			//退出当前的handler
+			return
+		}
+	}
 
 }
 
